f5ae2555d37e7f95f7a92a69ce13dfef102eb8f2
Noah Laptop <noah@koontzs.com>
2018-11-09 22:44:32 -0800

    re-remembering AWS cloudformation stuff!

--- PersonalSite/githubpoke.ts
+++ PersonalSite/githubpoke.ts
@@ -6,11 +6,27 @@
  * to be so awesome.
  */
 
-import { APIGatewayEvent, Callback, Context, Handler } from "aws-lambda";
+import * as AWS from "aws-sdk";
+import { Callback, Context, ScheduledEvent, ScheduledHandler } from "aws-lambda";
 import { queryGithubAPI } from "./githubQuery";
+
+/** Generate S3 client */
+const s3 = new AWS.S3();
+
+/** Generate parameters for the object we're grabbing from s3 */
+const s3params = {
+    Bucket: process.env.bucketName,
+    Key: process.env.fileName,
+};
 
 /**
  * The main function handler for this API.
  * Pokes the Github API frequently and compares the returned file against a cached one in S3
  * If they're different, then poke my IoT device as well.
  */
+export const githubcount: ScheduledHandler = async (event: ScheduledEvent, context: Context, cb: Callback) => {
+    // query the GraphQL API, and fetch our cached data from s3
+    const data = await s3.getObject(s3params).promise();
+    if (!data) console.log("No Data Found!");
+    else console.log(data.Body);
+};
