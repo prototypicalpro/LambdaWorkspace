a856957dd197d995476f0c7ed04e9789cca6cbfc
Noah Laptop <noah@koontzs.com>
2018-11-11 12:00:48 -0800

    fixed s3 cloudformation, now splitting permissions into different files (is less messy than when I did this last but still pretty messy)

--- PersonalSite/githubpoke.ts
+++ PersonalSite/githubpoke.ts
@@ -14,8 +14,18 @@ import { queryGithubAPI } from "./githubQuery";
 const s3 = new AWS.S3();
 
 /** Generate parameters for the object we're grabbing from s3 */
-const s3params = {
+const s3ParamsGet: AWS.S3.GetObjectRequest = {
     Bucket: process.env.bucketName,
+    Key: process.env.fileName,
+    ResponseContentEncoding: "utf8",
+    ResponseContentType: "application/json",
+};
+
+/** Generate parameters for the object when we're storing it in s3 */
+const s3ParamsPut: AWS.S3.PutObjectRequest = {
+    Bucket: process.env.bucketName,
+    ContentEncoding: "utf8",
+    ContentType: "application/json",
     Key: process.env.fileName,
 };
 
@@ -24,9 +34,27 @@ const s3params = {
  * Pokes the Github API frequently and compares the returned file against a cached one in S3
  * If they're different, then poke my IoT device as well.
  */
-export const githubcount: ScheduledHandler = async (event: ScheduledEvent, context: Context, cb: Callback) => {
+export const githubpoke: ScheduledHandler = async (event: ScheduledEvent, context: Context, cb: Callback) => {
+    // query the s3 API, returning a JSON object if we have a file, and null if we don't
+    // add the catch statement b/c s3 throws permission denied if the file doesn't exist
+    const s3Query = s3.getObject(s3ParamsGet).promise().then((r) => {
+        const str = (r.Body as Buffer).toString("utf8");
+        // both the string and data is useful to us, so return them both
+        return {
+            data: JSON.parse(str),
+            string: str,
+        };
+    }).catch(() => null);
     // query the GraphQL API, and fetch our cached data from s3
-    const data = await s3.getObject(s3params).promise();
-    if (!data) console.log("No Data Found!");
-    else console.log(data.Body);
+    const data = await Promise.all([s3Query, queryGithubAPI()]);
+    const s3Data: { str: string, data: AWS.S3.GetObjectOutput } | null = data[0];
+    const githubData = data[1];
+    // if we don't have github data, something is very wrong
+    if (!githubData) throw new Error(`Github data returned ${githubData}`);
+    // if we don't have s3 data
+    else if (!s3Data) {
+        // populate the bucket with our respose
+        const putPromsie = s3.putObject(Object.assign({ Body: s3Data.str }, s3ParamsPut)).promise();
+    }
+    else console.log((s3Data.Body as Buffer).toString("utf8"));
 };
---
+++ PersonalSite/iotpoke.ts
@@ -0,0 +1,10 @@
+/**
+ * IoT MQTT helper functions
+ * Provides methods to simplify poking my esp8266 using the AWS IoT framework
+ */
+
+ /** the interface dectribing the structure of the topic this file will publish to */
+ interface ITopic {
+     /** my total number of github contribution */
+     totalContrib: number;
+ }
